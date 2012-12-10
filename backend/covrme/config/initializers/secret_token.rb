df6b63ce5efb9c5134ef83b30b7155ac538b0b4e
Andy Pincombe <andy@pincom.be>
2012-12-10 03:48:32 -0500

    Add RAILS::API initial install

---
+++ backend/covrme/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Covrme::Application.config.secret_token = '520d79e39ff5661c8b1cc44712d184becc8856a41cd24b58173d9292fc6ab4db9f8b4d772fb637369143015afad07c66fddd4957f884a2a5969235fb3f932d93'
